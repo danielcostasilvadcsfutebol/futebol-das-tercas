31715a107a476c076682299f54b394cdba90b80f
danielcostasilvadcsfutebol <danielcostasilva.dcs.futebol@gmail.com>
2026-04-18 02:29:19 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  experimental: {
+    serverComponentsExternalPackages: ['web-push'],
+  },
+}
 
 module.exports = nextConfig
